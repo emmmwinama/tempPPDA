268e3225eddf85706491365c8d2c783d91eb9b56
Shift <shift@laravelshift.com>
2025-04-19 17:02:40 +0000

    Adopt short array syntax
    Since PHP 5.4 the short array syntax `[]` may be used instead of `array()`.

--- app/Http/Controllers/ContactFormController.php
+++ app/Http/Controllers/ContactFormController.php
@@ -19,13 +19,13 @@ class ContactFormController extends Controller
 
         Contact::create($request->all());
 
-        Mail::send('mail', array(
+        Mail::send('mail', [
             'name' => $request->get('name'),
             'email' => $request->get('email'),
             'phone' => $request->get('phone'),
             'subject' => $request->get('subject'),
             'userQuery' => $request->get('message'),
-        ), function ($message) use ($request){
+        ], function ($message) use ($request){
             $message->from($request->get('email'));
             $message->to('emmanuel.mwinama@ppda.mw', 'Admin')->subject($request->get('subject'));
         });
--- app/Http/Controllers/SupplierController.php
+++ app/Http/Controllers/SupplierController.php
@@ -8,43 +8,43 @@ class SupplierController extends Controller
 {
     //For handling supplier data
     function showRegisteredSuppliers(){
-        $suppliersdata = array(
-            array(1,"BQ Associates", "2023-12-01"),
-            array(2,"BQ Associates & BQ Associates & BQ Associates & BQ Associates & BQ Associates", "2023-12-01"),
-            array(3,"BQ Associates BQ Associates & BQ Associates & BQ Associates", "2023-12-01"),
-            array(4,"BQ Associates BQ Associates & BQ Associates & BQ Associates", "2023-12-01"),
-            array(5,"BQ Associates & BQ Associates & BQ Associates", "2023-12-01"),
-            array(6,"BQ Associates", "2023-12-01"),
-            array(7,"BQ Associates", "2023-12-01"),
-            array(8,"BQ Associates", "2023-12-01"),
-            array(9,"BQ Associates", "2023-12-01"),
-            array(10,"BQ Associates", "2023-12-01"),
-            array(11,"BQ Associates", "2023-12-01"),
-            array(12,"BQ Associates", "2023-12-01"),
-            array(13,"BQ Associates", "2023-12-01"),
-            array(14,"BQ Associates", "2023-12-01"),
-        );
+        $suppliersdata = [
+            [1,"BQ Associates", "2023-12-01"],
+            [2,"BQ Associates & BQ Associates & BQ Associates & BQ Associates & BQ Associates", "2023-12-01"],
+            [3,"BQ Associates BQ Associates & BQ Associates & BQ Associates", "2023-12-01"],
+            [4,"BQ Associates BQ Associates & BQ Associates & BQ Associates", "2023-12-01"],
+            [5,"BQ Associates & BQ Associates & BQ Associates", "2023-12-01"],
+            [6,"BQ Associates", "2023-12-01"],
+            [7,"BQ Associates", "2023-12-01"],
+            [8,"BQ Associates", "2023-12-01"],
+            [9,"BQ Associates", "2023-12-01"],
+            [10,"BQ Associates", "2023-12-01"],
+            [11,"BQ Associates", "2023-12-01"],
+            [12,"BQ Associates", "2023-12-01"],
+            [13,"BQ Associates", "2023-12-01"],
+            [14,"BQ Associates", "2023-12-01"],
+        ];
 
         return view('registered-suppliers', ['suppliers' => $suppliersdata]);
     }
 
     function showDebarredSuppliers(){
-        $suppliersdata = array(
-            array(1,"BQ Associates", "2023-12-01"),
-            array(2,"BQ Associates & BQ Associates & BQ Associates & BQ Associates & BQ Associates", "2023-12-01"),
-            array(3,"BQ Associates BQ Associates & BQ Associates & BQ Associates", "2023-12-01"),
-            array(4,"BQ Associates BQ Associates & BQ Associates & BQ Associates", "2023-12-01"),
-            array(5,"BQ Associates & BQ Associates & BQ Associates", "2023-12-01"),
-            array(6,"BQ Associates", "2023-12-01"),
-            array(7,"BQ Associates", "2023-12-01"),
-            array(8,"BQ Associates", "2023-12-01"),
-            array(9,"BQ Associates", "2023-12-01"),
-            array(10,"BQ Associates", "2023-12-01"),
-            array(11,"BQ Associates", "2023-12-01"),
-            array(12,"BQ Associates", "2023-12-01"),
-            array(13,"BQ Associates", "2023-12-01"),
-            array(14,"BQ Associates", "2023-12-01"),
-        );
+        $suppliersdata = [
+            [1,"BQ Associates", "2023-12-01"],
+            [2,"BQ Associates & BQ Associates & BQ Associates & BQ Associates & BQ Associates", "2023-12-01"],
+            [3,"BQ Associates BQ Associates & BQ Associates & BQ Associates", "2023-12-01"],
+            [4,"BQ Associates BQ Associates & BQ Associates & BQ Associates", "2023-12-01"],
+            [5,"BQ Associates & BQ Associates & BQ Associates", "2023-12-01"],
+            [6,"BQ Associates", "2023-12-01"],
+            [7,"BQ Associates", "2023-12-01"],
+            [8,"BQ Associates", "2023-12-01"],
+            [9,"BQ Associates", "2023-12-01"],
+            [10,"BQ Associates", "2023-12-01"],
+            [11,"BQ Associates", "2023-12-01"],
+            [12,"BQ Associates", "2023-12-01"],
+            [13,"BQ Associates", "2023-12-01"],
+            [14,"BQ Associates", "2023-12-01"],
+        ];
 
         return view('debarred-suppliers', ['suppliers' => $suppliersdata]);
     }
